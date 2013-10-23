1487c3352a3c03a72a9ce1c6b686e4d786a07a4c
Calum J. Eadie <calum@calumjeadie.com>
2013-10-23 14:28:57 +0100

    Set up vagrant to install ruby on rails

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ basic-rails-heroku-app
 ======================
 
 A really basic Rails app to experiment with Rails, Vagrant and Heroku.
+
+Resources
+---------
+
+http://docs.vagrantup.com/v2/getting-started/index.html
+
+http://ruby.railstutorial.org/ruby-on-rails-tutorial-book
+
+https://devcenter.heroku.com/articles/getting-started-with-rails4
---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# References:
+#
+# http://docs.vagrantup.com/v2/getting-started/provisioning.html
+# http://ruby.railstutorial.org/ruby-on-rails-tutorial-book#sec-rubygems
+
+apt-get update
+apt-get install -y vim curl
+
+# Install Heroku Toolbelt.
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# Install RVM for managing Ruby versions.
+curl -L https://get.rvm.io | bash -s
+
+# Install RVM dependancies.
+rvm requirements
+
+# Install Ruby.
+rvm install ruby
+
+# Use version just install by default.
+rvm use ruby --default
+
+# Install RubyGems.
+rvm rubygems current
+
+# Install Rails.
+gem install rails
